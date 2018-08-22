bdddc71a98a986015c69f753b31b1dcf6839ab0a
Petr <petr.gulchuk@gmail.com>
2018-08-23 00:41:21 +0200

    change cros-origen params

--- src/main/java/com/currency_exchange/config/WebMvcConfigProd.java
+++ src/main/java/com/currency_exchange/config/WebMvcConfigProd.java
@@ -3,6 +3,7 @@ package com.currency_exchange.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,12 +12,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+@Profile("prod")
+public class WebMvcConfigProd implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins("http://excurrate.com:3000")
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowCredentials(true).maxAge(3600);
   }
