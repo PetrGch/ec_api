8864c2587a2cf8347072c296fcb4771696a30355
Гульчук Петр <Petr.Gulchuk@gmail.com>
2018-09-09 11:50:45 +0200

    Update WebMvcConfigProd.java

--- src/main/java/com/currency_exchange/config/WebMvcConfigProd.java
+++ src/main/java/com/currency_exchange/config/WebMvcConfigProd.java
@@ -18,7 +18,7 @@ public class WebMvcConfigProd implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://excurrate.com:3000")
+        .allowedOrigins("http://excurrate.com:3000", "http://localhost:3000")
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowCredentials(true).maxAge(3600);
   }
